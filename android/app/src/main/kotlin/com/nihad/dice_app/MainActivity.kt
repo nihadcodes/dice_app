65323b4efd54371353574c1abd4b40b52cd1d1ec
nihadcodes <nihad.exe@gmail.com>
2024-10-12 21:13:48 +0600

    functionality done

---
+++ android/app/src/main/kotlin/com/nihad/dice_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nihad.dice_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'gradient_container.dart';
+
+void main() {
+  runApp(
+      const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+
+
+      body: GradientContainer(
+        Color.fromARGB(255, 33, 5, 109),
+        Color.fromARGB(255, 33, 5, 109),
+      )
+    ),
+  ),
+  );
+}
+
+
+
